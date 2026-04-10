fb33bb7728e30122818eaea84e205eab1b0f9e56
David <akinollaafeni212@gmail.com>
2026-04-10 17:33:33 +0000

    fix: add missing database.py module

---
+++ database.py
@@ -0,0 +1,8 @@
+﻿import os
+from sqlalchemy import create_engine as _ce, text as _text
+
+_DB_URL = os.environ.get("DATABASE_URL", "")
+if _DB_URL:
+    _engine = _ce(_DB_URL)
+else:
+    _engine = None
